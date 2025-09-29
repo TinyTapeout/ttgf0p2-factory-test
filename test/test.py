htfab/tt08-factory-test

feat: factory test module

test/test.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: MIT
 
 import cocotb
 from cocotb.clock import Clock
@@ -7,7 +7,7 @@ from cocotb.triggers import ClockCycles
 
 
 @cocotb.test()
-async def test_project(dut):
+async def test_loopback(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
@@ -17,24 +17,56 @@ async def test_project(dut):
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
+
+    # ui_in[0] == 0: Output is uio_in
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    for i in range(256):
+        dut.uio_in.value = i
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == i
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    # When under reset: Output is uio_in, uio is in input mode
+    dut.rst_n.value = 0
     await ClockCycles(dut.clk, 1)
+    assert dut.uio_oe.value == 0
+    for i in range(256):
+        dut.ui_in.value = i
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == i
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+@cocotb.test()
+async def test_counter(dut):
+    dut._log.info("Start")
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
+
+    # ui_in[0] == 1: bidirectional outputs enabled, put a counter on both output and bidirectional pins
+    dut.ui_in.value = 1
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 2)
+
+    dut._log.info("Testing counter")
+    for i in range(256):
+        assert dut.uo_out.value == dut.uio_out.value
+        assert dut.uo_out.value == i
+        await ClockCycles(dut.clk, 1)
+
+    dut._log.info("Testing reset")
+    for i in range(5):
+        assert dut.uo_out.value == i
+        await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 2)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0

TinyTapeout/ttgf0p2-factory-test

test/test.py
@@ -7,7 +7,7 @@ from cocotb.triggers import ClockCycles
 
 
 @cocotb.test()
-async def test_project(dut):
+async def test_loopback(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
@@ -17,24 +17,56 @@ async def test_project(dut):
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
+
+    # ui_in[0] == 0: Output is uio_in
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    for i in range(256):
+        dut.uio_in.value = i
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == i
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    # When under reset: Output is uio_in, uio is in input mode
+    dut.rst_n.value = 0
     await ClockCycles(dut.clk, 1)
+    assert dut.uio_oe.value == 0
+    for i in range(256):
+        dut.ui_in.value = i
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == i
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+@cocotb.test()
+async def test_counter(dut):
+    dut._log.info("Start")
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Set the clock period to 10 us (100 KHz)
+    clock = Clock(dut.clk, 10, unit="us")
+    cocotb.start_soon(clock.start())
+
+    # ui_in[0] == 1: bidirectional outputs enabled, put a counter on both output and bidirectional pins
+    dut.ui_in.value = 1
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 2)
+
+    dut._log.info("Testing counter")
+    for i in range(256):
+        assert dut.uo_out.value == dut.uio_out.value
+        assert dut.uo_out.value == i
+        await ClockCycles(dut.clk, 1)
+
+    dut._log.info("Testing reset")
+    for i in range(5):
+        assert dut.uo_out.value == i
+        await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 2)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0